38405465b6d85a6cde41af0750c055367cabda11
Abiodun <aabiodunawoyemi@gmail.com>
2022-05-27 21:13:13 +0100

    dependency issue solved

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -12,7 +12,7 @@ const Dashboard = () => {
     useEffect(() => {
         TotalInterest()
         TotalAmount()
-    }, [contributors])
+    })
 
     useEffect(() => {
         getDataFromLocalStorage()
